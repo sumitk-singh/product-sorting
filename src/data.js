e98291b0f1163168dc3bb730626b795a2620776f
SUMIT SINGH <sumit.email.dev@gmail.com>
2022-10-29 01:17:04 +0000

    added db

---
+++ src/data.js
@@ -0,0 +1,43 @@
+export default [
+    {
+        id: 1,
+        tittle: "iphone 12 mini",
+        price: "48000",
+        rating: "4/5"
+    },
+
+    {
+        id: 2,
+        tittle: "Samsung TV",
+        price: "35000",
+        rating: "3.4/5"
+    },
+
+    {
+        id: 3,
+        tittle: "Milton bottle",
+        price: "750",
+        rating: "4/5"
+    },
+
+    {
+        id: 4,
+        tittle: "HP Laptop",
+        price: "67000",
+        rating: "4/5"
+    },
+
+    {
+        id: 5,
+        tittle: "Intex wwireless mouse",
+        price: "1200",
+        rating: "4.6/5"
+    },
+
+    {
+        id: 5,
+        tittle: "Sunglass",
+        price: "894",
+        rating: "4.2/5"
+    },
+]
